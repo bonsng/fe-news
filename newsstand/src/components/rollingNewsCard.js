0da86e835dd3e018e7ed7e389e4fc08eeb23e163
bonsng <john.k7795@gmail.com>
2025-12-30 16:48:46 +0900

    style: 롤링 파트 css 설정

--- newsstand/src/components/rollingNewsCard.js
+++ newsstand/src/components/rollingNewsCard.js
@@ -1,11 +1,15 @@
 export const rollingNewsCard = ({ title = "연합뉴스", text = "dummy" }) => {
   const cardWrapper = document.createElement("div");
   cardWrapper.id = "rolling-card-wrapper";
+  cardWrapper.classList.add("border-default");
+  cardWrapper.classList.add("surface-alt");
 
   const newsTitle = document.createElement("h1");
+  newsTitle.classList.add("typo-display-bold14");
   newsTitle.innerText = title;
 
   const newsContent = document.createElement("p");
+  newsContent.classList.add("typo-available-medium14");
   newsContent.innerText = text;
 
   cardWrapper.append(newsTitle);
