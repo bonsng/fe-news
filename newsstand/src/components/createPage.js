5dcf1c084d75247bdd0c1842e04acaa7c9867850
bonsng <john.k7795@gmail.com>
2025-12-30 21:59:25 +0900

    refacor: 명료성을 위한 구조 리팩터링

--- newsstand/src/components/createPage.js
+++ newsstand/src/components/createPage.js
@@ -1,5 +1,8 @@
 import { createEl } from "../lib/dom";
+import { createHeader } from "./createHeader";
 
 export const createPage = () => {
   const main = createEl("div", "ns-main-container");
+  main.appendChild(createHeader());
+  return main;
 };
