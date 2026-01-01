aa3c0cb974ae675c8e9d177b220b2c2fb16a3f80
bonsng <john.k7795@gmail.com>
2026-01-01 21:19:58 +0900

    feat: 상태변화에 따른 리렌더를 위해 렌더링 함수 구독

--- newsstand/src/app.js
+++ newsstand/src/app.js
@@ -1,5 +1,6 @@
 import { createPage } from "./components/createPage";
 import { render } from "./lib/dom";
+import { store } from "./state/store";
 import "./styles/foundation.css";
 import "./styles/object.css";
 import "./styles/typo.css";
@@ -7,3 +8,8 @@ import "./styles/typo.css";
 const $root = document.querySelector("#app");
 
 render($root, createPage);
+
+// Re-render on state changes
+store.subscribe(() => {
+  render($root, createPage);
+});
