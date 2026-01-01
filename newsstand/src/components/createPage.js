4b6361b47268548096c444f473767d2ef5cc2b46
bonsng <john.k7795@gmail.com>
2026-01-01 15:41:35 +0900

    feature: contents (controls, grid) 파트 레이아웃 추가

--- newsstand/src/components/createPage.js
+++ newsstand/src/components/createPage.js
@@ -1,9 +1,10 @@
 import { createEl } from "../lib/dom";
 import { createHeader } from "./createHeader";
+import { createNewsContents } from "./createNewsContents";
 import { createRolling } from "./createRolling";
 
 export const createPage = () => {
   const main = createEl("div", "ns-main-container");
-  main.append(createHeader(), createRolling());
+  main.append(createHeader(), createRolling(), createNewsContents());
   return main;
 };
