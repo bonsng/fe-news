a12cf4f131df707f88fb5fb73a947af8fda6c2d7
bonsng <john.k7795@gmail.com>
2026-01-04 12:15:44 +0900

    refactor: 레이아웃 재설정 (롤링하는 트랙을 리스트로 변경)

--- newsstand/src/components/createRolling.js
+++ newsstand/src/components/createRolling.js
@@ -2,20 +2,49 @@ import { createEl } from "../lib/dom";
 import { getRollingData } from "../lib/pressData";
 import { createRollingNewsCard } from "./createRollingNewsCard";
 
+const ROLLING_NEWS_COUNT = 5;
+
+let leftTrackState;
+let rightTrackState;
+
 export const createRolling = () => {
-  const rollingWrapper = createEl("section", "", "");
+  const rollingWrapper = createEl(
+    "section",
+    "",
+    `
+    <div id="left-rolling-list" class="border-default surface-alt rolling-list rolling-left">
+      <ul class="rolling-track"></ul>
+    </div>
+    <div id="right-rolling-list" class="border-default surface-alt rolling-list rolling-right">
+      <ul class="rolling-track"></ul>
+    </div>
+  `
+  );
   rollingWrapper.id = "roll-wrapper";
 
-  const rollingData = [...getRollingData()];
+  const [leftRollingData, rightRollingData] = [
+    ...getRollingData({ left: ROLLING_NEWS_COUNT, right: ROLLING_NEWS_COUNT }),
+  ];
 
-  for (let i = 0; i < 2; i++) {
-    const card = createRollingNewsCard(
-      rollingData[i].pressName,
-      rollingData[i].title.slice(0, 36)
-    );
-    card.dataset.link = rollingData[i].url;
-    rollingWrapper.append(card);
-  }
+  const leftTrack = rollingWrapper.querySelector(
+    ".rolling-left .rolling-track"
+  );
+  const rightTrack = rollingWrapper.querySelector(
+    ".rolling-right .rolling-track"
+  );
+  leftTrackState = setUpTrack(leftTrack, leftRollingData, 2);
+  rightTrackState = setUpTrack(rightTrack, rightRollingData, 2);
 
   return rollingWrapper;
+};
+
+const setUpTrack = (track, data, trackCount) => {
+  track.innerHTML = "";
+
+  data.slice(0, trackCount).forEach((item) => {
+    const newsCard = createRollingNewsCard(item.pressName, item.title);
+    track.appendChild(newsCard);
+  });
+
+  return { topIdx: 0 };
 };
--- newsstand/src/components/createRollingNewsCard.js
+++ newsstand/src/components/createRollingNewsCard.js
@@ -2,14 +2,13 @@ import { createEl } from "../lib/dom";
 
 export const createRollingNewsCard = (title = "연합뉴스", text = "dummy") => {
   const rollingCard = createEl(
-    "div",
-    "border-default surface-alt",
+    "li",
+    "rolling-item",
     `
-      <h1 class="typo-display-bold14">${title}</h1>
-      <p class="typo-available-medium14">${text}</p>
+      <span class="typo-display-bold14 news-provider">${title}</span>
+      <span class="typo-available-medium14 news-headline">${text}</span>
     `
   );
-  rollingCard.id = "rolling-card-wrapper";
 
   return rollingCard;
 };
