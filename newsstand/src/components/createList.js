483a9c7bf728bc3821ea8cd3498f34ead1844acc
bonsng <john.k7795@gmail.com>
2026-01-06 11:49:55 +0900

    feat: 리스트 뷰 초기 레이아웃 설정 - `createList.js` 	- fetch 로 mock data 를 가져와 카테고리별로 분류하고, 요구사항에 맞게 레이아웃 구성 	- `createList` 에서 데이터를 모두 받아와 props 로 내려주는 구조 - `pressData.js` 	- 카테고리별로 신문사들을 재가공하는 함수 구현 - `pressMockData.json` 	- 요구사항에 맞게 텍스트 편집

--- newsstand/src/components/createList.js
+++ newsstand/src/components/createList.js
@@ -1,15 +1,100 @@
 import { createEl } from "../lib/dom";
+import { groupPressByCategory, PRESS_CATEGORIES } from "../lib/pressData";
+
+let listPressData;
 
 export const createList = () => {
-  const nsList = createEl(
-    "section",
-    "ns-press-list border-default",
-    `
-      <div class="ns-press-list__list">리스트 컴포넌트</div>
-    `
-  );
+  const nsList = createEl("section", "ns-press-list border-default", "");
   nsList.classList.add("ns-press-list");
   nsList.setAttribute("aria-label", "언론사 목록");
 
+  fetch("/pressMockData.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch mock data");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      listPressData = groupPressByCategory(data);
+      return listPressData;
+    })
+    .then((groupedData) => {
+      nsList.append(createListNav(), createListContent(groupedData));
+    })
+    .catch((err) => console.error(err));
+
   return nsList;
+};
+
+const createListNav = () => {
+  const nav = createEl(
+    "nav",
+    "ns-press-list__nav",
+    `<ul class="ns-press-list__nav-list"></ul>`
+  );
+  const navList = nav.querySelector(".ns-press-list__nav-list");
+  PRESS_CATEGORIES.forEach((category) => {
+    navList.append(createNavItem(category));
+  });
+
+  return nav;
+};
+
+const createNavItem = (category) => {
+  const navItem = createEl("li", "ns-press-list__nav-item", category);
+  return navItem;
+};
+
+const createListContent = (groupedData) => {
+  let nowCategory = PRESS_CATEGORIES[0];
+  let nowPress = groupedData[nowCategory].presses[0];
+  const listContentWrapper = createEl(
+    "div",
+    "ns-press-list__content-wrapper",
+    ""
+  );
+
+  const listContentHeader = createEl(
+    "div",
+    "ns-press-list__content-header",
+    `<img src="${
+      nowPress.logo
+    }" alt="뉴스아이콘" class="ns-press-list__content-header-icon" />
+    <span>${new Date().getFullYear()}년 ${nowPress.time}</span>
+    <span>구독버튼</span>
+    `
+  );
+  const listNews = createEl(
+    "div",
+    "ns-press-list__content",
+    `<div class="ns-press-list__main-news">
+      <img src="${nowPress.mainImg}" alt="메인뉴스이미지" class="ns-press-list__main-news-image" />
+      <span class="ns-press-list__main-news-title">${nowPress.mainTitle}</span>
+    </div>
+    <ul class="ns-press-list__sub-news"></ul>
+    `
+  );
+
+  const subNewsContainer = listNews.querySelector(".ns-press-list__sub-news");
+  subNewsContainer.append(
+    ...nowPress.relatedArticles.map((article) => {
+      return createEl(
+        "li",
+        "ns-press-list__sub-news-title",
+        `${article.title}`
+      );
+    })
+  );
+  subNewsContainer.append(
+    createEl(
+      "li",
+      "ns-press-list__sub-news-edited",
+      `${nowPress.press}에서 직접 편집한 뉴스입니다.`
+    )
+  );
+
+  listContentWrapper.append(listContentHeader, listNews);
+
+  return listContentWrapper;
 };
